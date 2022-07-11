ee1c5d563aeb7881e8c2f20a2c812729cfa2374a
sudais <127001sudais01@gmail.com>
2022-07-11 15:01:16 +0800

    finisher redux action.js ,initialized reducer.js

--- todo/redux/action.js
+++ todo/redux/action.js
@@ -47,3 +47,145 @@ export let addTask = (title, description) => async (dispatch) => {
         dispatch({ type: "addTaskFailure", payload: err.response.data.message })
     }
 }
+
+export let updateTask = (taskId) => async (dispatch) => {
+    try {
+        dispatch({ type: "updateTaskRequest" })
+
+        let { data } = await axios.get(`${serverUrl}/task/${taskId}`)
+        dispatch({ type: "updateTaskSuccess", payload: data.message })
+
+    } catch (err) {
+        dispatch({ type: "updateTaskFailure", payload: err.response.data.message })
+
+    }
+}
+
+export let deleteTask = (taskId) => async (dispatch) => {
+    try {
+        dispatch({ type: "deleteTaskRequest" })
+
+        let { data } = await axios.delete(`${serverUrl}/task/${taskId}`)
+        dispatch({ type: "deleteTaskSuccess", payload: data.message })
+    } catch (err) {
+        dispatch({
+            type: "deleteTaskFailure",
+            payload: err.response.data.message
+        })
+    }
+}
+
+export let updateProfile = (formData) => async (dispatch) => {
+    try {
+        dispatch({ type: "updateProfileRequest" })
+
+        let { data } = await axios.put(`${serverUrl}/updateprofile`, formData, {
+            headers: {
+                "Content-Type": "multipart/form-data"
+            }
+        })
+        dispatch({ type: "updateProfileSuccess", payload: data.message })
+    } catch (err) {
+        dispatch({
+            type: "updateProfileFailure",
+            payload: err.response.data.message
+        })
+    }
+}
+
+export let logout = () => async (dispatch) => {
+    try {
+        dispatch({ type: "logoutRequest" })
+
+        await axios.get(`${serverUrl}/logout`)
+        dispatch({ type: "logoutSuccess" })
+
+    } catch (err) {
+        dispatch({
+            type: "logoutFailure",
+            payload: err.response.data.message
+        })
+    }
+}
+
+export let register = (formData) => async (dispatch) => {
+    try {
+        dispatch({ type: "registerRequest" })
+
+        let { data } = await axios.post(`${serverUrl / register}`, formData, {
+            headers: { "Content-Type": "multipart/form-data" },
+        })
+        dispatch({ type: "registerSuccess", payload: data })
+    } catch (err) {
+        dispatch({
+            type: "registerFailure",
+            payload: err.response.data.message
+        })
+    }
+}
+
+export let updatePassword = (oldPassword, newPassword) => async (dispatch) => {
+    try {
+        dispatch({ type: "updatePasswordRequest" })
+
+        let { data } = await axios.put(`${serverUrl}/updatepassword`, { oldPassword, newPassword },
+            {
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            })
+        dispatch({ type: "updatePasswordSuccess", payload: data.message })
+    } catch (err) {
+        dispatch({
+            type: "updatePasswordFailure",
+            payload: err.response.data.message
+        })
+
+    }
+}
+
+export let verify = (otp) => async (dispatch) => {
+    try {
+        dispatch({ type: "verificationRequest" })
+
+        let { data } = await axios.post(`${serverUrl}/verify`, { otp }, {
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        dispatch({ type: "verificationSuccess", payload: data.message })
+    } catch (err) {
+        dispatch({ type: "verificationFailure", payload: err.response.data.message })
+    }
+}
+
+export let forgetPassword = (email) => async (dispatch) => {
+    try {
+        dispatch({ type: "forgetPasswordRequest" })
+
+        let { data } = await axios.post(`${serverUrl}/forgetpassword`, { email }, {
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        dispatch({ type: "forgetPasswordSuccess", payload: data.message })
+    } catch (err) {
+        dispatch({
+            type: "forgetPasswordFailure",
+            payload: err.response.data.message
+        })
+    }
+}
+
+export let resetPassword = (otp, newPassword) => async (dispatch) => {
+    try {
+        dispatch({ type: "resetPasswordRequest" })
+
+        let { data } = await axios.put(`${serverUrl}/resetpassword`, { otp, newPassword }, {
+            headers: { "Content-Type": "application/json", }
+        })
+        dispatch({ type: "resetPasswordSuccess", payload: data.message })
+    } catch (err) {
+        dispatch({ type: "resetPasswordFailure", payload: err.response.data.message })
+    }
+}
